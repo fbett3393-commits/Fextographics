cd795e83272662e51d8cd5ac4ce8bc0954a09ee5
fbett3393-commits <fbett3393@gmail.com>
2025-11-11 08:45:46 +0300

    Create Fextographicswebsite.c

---
+++ Fextographicswebsite.c
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Fexto Graphics</title>
+  ...
+</head>
+<body>
+  <h1>Welcome to Fexto Graphics</h1>
+  <p>Creative posters that speak for you.</p>
+</body>
+</html>
